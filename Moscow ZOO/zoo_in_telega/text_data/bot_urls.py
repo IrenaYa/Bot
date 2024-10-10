494bcfd70f5114d878fb72586a077213214e9901
IrenaYa <159187460+IrenaYa@users.noreply.github.com>
2024-10-10 20:27:37 +0300

    Add files via upload

---
+++ Moscow ZOO/zoo_in_telega/text_data/bot_urls.py
@@ -0,0 +1,4 @@
+CLUB_FRIENDS_SITE = 'https://moscowzoo.ru/my-zoo/become-a-guardian/?utm_source=telegram-quiz-bot&utm_medium=messenger&utm_campaign=care-program'
+
+
+MOSCOW_ZOO_ANIMALS = 'https://www.moscowzoo.ru/animals/'
